ab133c279d531fc38aeabc0bb7591b7bc6c0da1d
Hüseyin <hkrabs@yandex.com>
2023-02-28 12:31:37 +0300

    Readme, screenshot, Github icon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Guitar Fret Calculator
 
+[Website](https://yinkar.github.io/guitar-fret-calculator)
+
+## Screenshot
+![Screenshot](screenshot.png)
--- src/App.vue
+++ src/App.vue
@@ -57,6 +57,12 @@ onMounted(() => {
     </div>
 
     <Result :frets="frets" :scale-length="scaleLength" :precision="precision" />
+
+    <div class="icons">
+      <a href="https://github.com/yinkar/guitar-fret-calculator" title="Github Repo">
+        <img src="./assets/icons/github.svg" alt="Github Repo">
+      </a>
+    </div>
   </div>
 
 </template>
@@ -94,5 +100,31 @@ onMounted(() => {
   border-radius: 5px;
   outline: none;
   width: 100px;
+}
+.icons {
+  position: absolute;
+  width: 200px;
+  height: 40px;
+  right: 0;
+  top: 0;
+  display: flex;
+  justify-content: right;
+}
+
+.icons > a {
+  display: inline-block;
+  height: 60px;
+  margin: 10px 10px;
+  opacity: .6;
+  transition: 80ms ease-in-out all;
+}
+
+.icons > a:hover {
+  opacity: 1;
+}
+
+.icons img {
+  height: 40px;
+  width: auto;
 }
 </style>
