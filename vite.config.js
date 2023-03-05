4abb72bd35707caba73b755b83cd07be0a15a0e2
yinkar <hkrabs@yandex.com>
2023-03-05 06:30:29 +0300

    PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/guitar-fret-calculator/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+      },
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'icon-192.png',
+        'icon-512.png',
+      ],
+      manifest: {
+          "name": "Guitar Fret Calculator",
+          "short_name": "Guitar Fret Calculator",
+          "description": "Calculate guitar fret positions on fretboard to produce a guitar from scratch",
+          "theme_color": "#d37c39",
+          "background_color": "#242424",
+          "icons": [
+              {
+                  "src": "icon-192.png",
+                  "sizes": "192x192",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              },
+              {
+                  "src": "icon-512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              }
+          ],
+      },
+    }),
+  ],
 });
