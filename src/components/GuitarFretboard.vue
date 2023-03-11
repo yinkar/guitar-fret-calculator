2a10b81fcdc58a7d617bc494f271d1bb1fbb3d22
yinkar <hkrabs@yandex.com>
2023-03-11 16:29:04 +0300

    Right margin increase

--- src/components/GuitarFretboard.vue
+++ src/components/GuitarFretboard.vue
@@ -24,7 +24,7 @@ defineProps({
 .guitar-fretboard {
   height: 80px;
   position: relative;
-  margin: 0 280px; 
+  margin: 0 320px 0 280px;
   box-sizing: border-box;
   box-shadow: 0 4px 3px rgba(0, 0, 0, .3);
   filter: drop-shadow(0 40px 4px rgba(0, 0, 0, .3));
